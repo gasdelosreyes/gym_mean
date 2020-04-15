9f4bd6ed405afc4f7f18474abeb5ea3d53624499
mfacu17 <mfacu98@gmail.com>
2020-04-15 17:11:12 -0300

    Config DB 2

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const connectDB = require('./config/db')
 
+// Cargando variables de entorno
 dotenv.config({ path: './config/config.env' })
 
+// Conectando express
 const app = express()
 
+// Obteniendo variables de entorno
 const PORT = process.env.PORT || 3000
 
+// Conectando a la base de datos
+connectDB()
+
+// Conectando al servidor
 app.listen(PORT, console.log('Escuchando puerto', PORT))
