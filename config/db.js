c854aeea8fe7fb52dc0888dc20abb4d825c6a7d6
mfacu17 <mfacu98@gmail.com>
2020-04-19 21:13:44 -0300

    Db actualizado

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
-const mongoose = require('mongoose')
-
 const connectDB = async() => {
     let connect = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
@@ -7,7 +5,7 @@ const connectDB = async() => {
         useCreateIndex: true,
         useUnifiedTopology: true
     })
-    console.log('MongoDB conectado:'.green.underline.bolds + ` ${connect.connection.host}`.yellow);
+    console.log(`\nConectado a la base de datos:`.cyan.underline.bold + `${connect.connections[0].host}`.yellow);
 }
 
 module.exports = connectDB;
